444b39bc3169ea964fb562ca0b103161673bcc20
Devvrat Shukla <devvratshukla@gmail.com>
2021-03-27 16:39:50 +0530

    chore: updated dependencies

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'consistent',
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  endOfLine: 'auto',
+};
--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="app">
-            <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo" />
-                <h4 className="app-title"> App Title</h4>
-            </header>
-            <main className="app-main"></main>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
